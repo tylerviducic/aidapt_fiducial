29d28286722f69e986de47b4c0cafd46230541c0
Tyler Viducic <tyler.viducic@gmail.com>
2020-07-21 11:36:10 -0400

    added file read-in

--- apply_fiducial.py
+++ apply_fiducial.py
@@ -2,4 +2,10 @@
 
 import numpy as np
 
-# Read in data here
+# Read in data here 
+
+data_file = '/media/tylerviducic/Elements/aidapt/synthetic/clasfilter2_5M780.npy' # change to your path, obviously
+
+data_array = np.load(data_file)
+
+print(data_array.tostring)
