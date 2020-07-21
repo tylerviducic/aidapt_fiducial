abce464a59356242c9a7af4b9ec7a3d79dedabdf
Tyler Viducic <tyler.viducic@gmail.com>
2020-07-21 14:13:00 -0400

    removed e vent array from datamanager init

--- dataManager.py
+++ dataManager.py
@@ -4,7 +4,6 @@ import numpy as np
 class DataManager:
 
     def __init__(self, event_array):
-        self.event_array = event_array
         self.proton = (event_array[2], event_array[5], event_array[8], event_array[11])
         self.pi_plus = (event_array[3], event_array[6], event_array[9], event_array[12])
         self.pi_minus = (event_array[4], event_array[7], event_array[10], event_array[13])        
